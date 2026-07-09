b98b6eb5dc321913ff5195fd0873cc15522b91c8
Farhan Khan <farhan@eng.rizvi.edu.in>
2026-07-09 09:51:51 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int high = 0;
+        int low =0 ;
+        int n = s1.length();
+        int m = s2.length();
+        HashMap<Character,Integer> freq =new HashMap<>();
+        HashMap<Character,Integer> window =new HashMap<>();
+        for(int i =0 ;i<n;i++){
+            char c = s1.charAt(i);
+            freq.put(c , freq.getOrDefault(c,0)+1);
+        }
+        while(high<m){
+            char c = s2.charAt(high);
+            window.put(c,window.getOrDefault(c,0)+1);
+            while(high-low+1>n){
+                char left = s2.charAt(low);
+                window.put(left,window.get(left)-1);
+                if(window.get(left)==0){
+                    window.remove(left);
+                }
+                low++;
+            }
+            if(high-low+1 == n){
+                if(window.equals(freq)){
+                    return true;
+                }
+            }
+            high++;
+        }
+        return false;
+    }
+}
