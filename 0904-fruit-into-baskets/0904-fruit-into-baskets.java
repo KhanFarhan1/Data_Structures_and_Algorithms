4264e1b00f90234a7b844dc3ba7ab65ad5e806d4
Farhan Khan <farhan@eng.rizvi.edu.in>
2026-07-13 08:51:36 +0530

    Time: 54 ms (67.08%), Space: 70.4 MB (56.73%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,22 +1,20 @@
 class Solution {
     public int totalFruit(int[] fruits) {
         int high = 0;
-        int low = 0;
+        int low =0;
+        int ans = -1;
         int n = fruits.length;
-        int ans = 0;
-        int sum =0;
-        HashMap<Integer,Integer> freq =new HashMap<>();
+        HashMap<Integer,Integer> freq=new HashMap<>();
         while(high<n){
-        freq.put(fruits[high] , freq.getOrDefault(fruits[high],0)+1);
-        while(freq.size() > 2){
-            freq.put(fruits[low] , freq.get(fruits[low])-1);
-            if(freq.get(fruits[low])==0){
-                freq.remove(fruits[low]);
+            freq.put(fruits[high],freq.getOrDefault(fruits[high],0)+1);
+            while(freq.size()>2){
+                freq.put(fruits[low],freq.get(fruits[low])-1);
+                if(freq.get(fruits[low])==0){
+                    freq.remove(fruits[low]);
+                }
+                low++;
             }
-            low++;
-        }
-        sum=high-low+1;
-        ans = Math.max(sum,ans);
+            ans = Math.max(ans,high-low+1);
             high++;
         }
         return ans;
