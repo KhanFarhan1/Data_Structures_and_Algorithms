6f00ef1ca1d1a227af80f3c7b90a1eed816b9de1
Farhan Khan <farhan@eng.rizvi.edu.in>
2026-07-14 13:12:41 +0530

    Time: 14 ms (60.56%), Space: 47 MB (53.07%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,41 +1,41 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int m = s.length();
-        int n = t.length();
-        int low = 0;
-        int high = 0;
-        int count = 0;
-        int minlen = Integer.MAX_VALUE;
-        int startIndex = -1;
-        HashMap<Character , Integer> freq = new HashMap<>();
-        for(int i = 0 ;i < n;i++){
-            char c = t.charAt(i);
-            freq.put(c , freq.getOrDefault(c,0)+1);
+       int m = s.length();
+       int n = t.length();
+       int high =0;
+       int low =0;
+       int startIdx = -1;
+       int len =Integer.MAX_VALUE;
+       int count = 0;
+       HashMap<Character,Integer> freq = new HashMap<>();
+       for(int i = 0;i<n;i++){
+        char c = t.charAt(i);
+        freq.put(c,freq.getOrDefault(c,0)+1);
+       }
+       while(high<m){
+        char c = s.charAt(high);
+        if(freq.getOrDefault(c,0)>0){
+            count++;
         }
-        while(high<m){
-            char c = s.charAt(high);
-            if(freq.getOrDefault(c,0)>0){
-                count++;
+        freq.put(c,freq.getOrDefault(c,0)-1);
+        while(count==n){
+            if(high-low+1 < len){
+                len = high-low+1;
+                startIdx = low;
             }
-            freq.put(c,freq.getOrDefault(c , 0)-1);
-            while(count == n){
-                if(high-low+1 < minlen){
-                    minlen = high-low+1;
-                    startIndex = low;
-                }
-                char left = s.charAt(low);
-                freq.put(left , freq.get(left)+1);
-                if(freq.getOrDefault(left , 0)>0){
-                    count--;
-                }
-                low++;
+            char left = s.charAt(low);
+            freq.put(left,freq.get(left)+1);
+            if(freq.getOrDefault(left,0)>0){
+                count--;
             }
-            high++;
+            low++;
         }
-        if(startIndex == -1){
-            return "";
-        }else{
-            return s.substring(startIndex,startIndex+minlen);
-            }
+        high++;
+       }
+       if(startIdx == -1){
+        return "";
+       }else{
+        return s.substring(startIdx,len+startIdx);
+       }
     }
 }
