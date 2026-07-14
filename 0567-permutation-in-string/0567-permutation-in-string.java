4718e3664943e2d80b2bceb6a6d5b553b35ea30b
Farhan Khan <farhan@eng.rizvi.edu.in>
2026-07-14 13:55:42 +0530

    Time: 23 ms (30.15%), Space: 46.5 MB (25.31%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,20 +1,17 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int n = s1.length();
-        int m = s2.length();
-        int high = 0;
+        int high = 0 ;
         int low = 0;
-        int count = 0;
-        HashMap<Character , Integer> freq = new HashMap<>();
-        HashMap<Character , Integer> window = new HashMap<>();
-        for(int i = 0;i<n;i++){
+        HashMap<Character,Integer> freq = new HashMap<>();
+        HashMap<Character,Integer> window = new HashMap<>();
+        for(int i =0;i<s1.length();i++){
             char c = s1.charAt(i);
             freq.put(c,freq.getOrDefault(c,0)+1);
         }
-        while(high<m){
+        while(high<s2.length()){
             char c = s2.charAt(high);
             window.put(c,window.getOrDefault(c,0)+1);
-            while(high-low+1 > n){
+            while(high-low+1 > s1.length()){
                 char left = s2.charAt(low);
                 window.put(left,window.get(left)-1);
                 if(window.get(left)==0){
@@ -22,12 +19,12 @@ class Solution {
                 }
                 low++;
             }
-            if(high-low+1==n){
+            if(high-low+1 == s1.length()){
                 if(window.equals(freq)){
                     return true;
                 }
             }
-           high++; 
+            high++;
         }
         return false;
     }
