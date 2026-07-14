e5ceb1f910cc399921c3c3c7167419bab7cc5a09
Farhan Khan <farhan@eng.rizvi.edu.in>
2026-07-14 14:19:21 +0530

    Time: 54 ms (24.99%), Space: 47.8 MB (15.52%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,20 +1,18 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int high = 0;
-        int low = 0;
-        int n = s.length();
-        int m = p.length();
+        int high=0;
+        int low =0;
         HashMap<Character,Integer> freq = new HashMap<>();
         HashMap<Character,Integer> window = new HashMap<>();
         List<Integer> ans = new ArrayList<>();
-        for(int i = 0;i<m;i++){
+        for(int i = 0;i<p.length();i++){
             char c = p.charAt(i);
             freq.put(c,freq.getOrDefault(c,0)+1);
         }
-        while(high<n){
+        while(high<s.length()){
             char c = s.charAt(high);
             window.put(c,window.getOrDefault(c,0)+1);
-            while(high-low+1 > m){
+            while(high-low+1>p.length()){
                 char left = s.charAt(low);
                 window.put(left,window.get(left)-1);
                 if(window.get(left)==0){
@@ -22,7 +20,7 @@ class Solution {
                 }
                 low++;
             }
-            if(high-low+1 == m){
+            if(high-low+1 == p.length()){
                 if(window.equals(freq)){
                     ans.add(low);
                 }
