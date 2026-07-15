974c4d8cb562f8b9ca0af40fb7bae7558b2beefb
Farhan Khan <farhan@eng.rizvi.edu.in>
2026-07-15 17:25:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        if(head == null ){
+        if(head == null || head.next == null){
             return null;
         }
         ListNode slow =head;
