b7f6c7a8e651e06601097f712d9a3e4d598ca4eb
Farhan Khan <farhan@eng.rizvi.edu.in>
2026-07-15 11:19:14 +0530

    Time: 0 ms (100%), Space: 42.4 MB (74.78%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,15 +1,16 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int sumOdd= 0;
-        int sumEven = 0;
-        for(int i = 0;i<2*n;i++){
-            if(i%2==0){
-                sumEven +=i;
-            }else{
-                sumOdd +=i;
-            }
-        }
-    int ans = Math.abs(sumOdd-sumEven);
-    return ans;
+    //     int sumOdd= 0;
+    //     int sumEven = 0;
+    //     for(int i = 0;i<2*n;i++){
+    //         if(i%2==0){
+    //             sumEven +=i;
+    //         }else{
+    //             sumOdd +=i;
+    //         }
+    //     }
+    // int ans = Math.abs(sumOdd-sumEven);
+    // return ans;
+    return n;
     }
 }
