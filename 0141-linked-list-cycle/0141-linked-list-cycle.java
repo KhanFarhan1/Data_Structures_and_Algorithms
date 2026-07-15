4173a3c830a3b8498103657760e9657c5ac44881
Farhan Khan <farhan@eng.rizvi.edu.in>
2026-07-15 16:03:51 +0530

    Time: 0 ms (100%), Space: 47 MB (15.94%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,20 +9,37 @@
  *     }
  * }
  */
+// public class Solution {
+//     public boolean hasCycle(ListNode head) {
+//         if(head == null || head.next == null){
+//             return false;
+//         }
+//         ListNode slow = head;
+//         ListNode fast =head;
+//         while(fast.next != null && fast.next.next != null){
+//             slow = slow.next;
+//             fast = fast.next.next;
+//             if(slow.next == fast.next){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null){
-            return false;
-        }
+        ListNode fast = head;
         ListNode slow = head;
-        ListNode fast =head;
-        while(fast.next != null && fast.next.next != null){
-            slow = slow.next;
+
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
-            if(slow.next == fast.next){
+            slow = slow.next;
+
+            if (fast == slow) {
                 return true;
             }
         }
-        return false;
+
+        return false;        
     }
 }
