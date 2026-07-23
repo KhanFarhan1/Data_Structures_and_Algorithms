9d1ab26b84c8797cc20e7b891a61004b551a295e
Farhan Khan <farhan@eng.rizvi.edu.in>
2026-07-24 02:35:26 +0530

    Time: 0 ms (100%), Space: 44.4 MB (26.24%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -8,31 +8,37 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+ /*
+ 1. find size of the l.l.
+ 2. now find right_postion where to split it 
+ for this there is technique k = k%size
+ 3. loop it till we go to right_postion 
+ 4 connect
+  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head == null || head.next ==null ){
+        if(head == null || head.next == null ){
             return head;
         }
-
-       int size =1 ;
-       ListNode temp = head;
-       while(temp.next != null){
-        temp=temp.next;
-        size++;
-       }
-       ListNode right_postion = head;
-       k = k%size;
-       if(k == 0){
-        return head;
-       }
-       int i= 0; 
-       while(i != size-k-1){
-        right_postion=right_postion.next;
-        i++;
-       }
-       ListNode newHead = right_postion.next;
-       right_postion.next = null;
-       temp.next = head;
-       return newHead;
+        ListNode temp = head;
+        int size =1;
+        while(temp.next != null ){
+            temp = temp.next;
+            size++;
+        }
+        k = k%size;
+        if(k == 0){
+            return head;
+        }
+        ListNode right_postion = head;
+        int i =0;
+        while(i!=size-k-1){
+            right_postion = right_postion.next;
+            i++;
+        }
+        ListNode newHead = right_postion.next;
+        right_postion.next = null;
+        temp.next = head;
+        return newHead;
     }
 }
