2f48894cc432500a08e4fb86da4d6c999667178f
Farhan Khan <farhan@eng.rizvi.edu.in>
2026-08-08 14:05:56 +0530

    Time: 1 ms (99.96%), Space: 51.2 MB (31.79%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -3,20 +3,20 @@ class Solution {
         if(goal<0){
             return 0;
         }
-       int high = 0;
-       int low = 0;
-       int sum = 0;
-       int count = 0;
-       while(high < nums.length){
-        sum = sum + nums[high];
-       while(sum>goal){
-        sum = sum - nums[low];
-        low++;
-       }
-       count = count + high-low+1;
-       high++;
-       }
-       return count;
+        int high =0;
+        int low = 0;
+        int sum =0;
+        int count =0;
+        while(high<nums.length){
+            sum = sum + nums[high];
+            while(sum>goal){
+                sum = sum - nums[low];
+                low++;
+            }
+            count = count + high-low+1;
+            high++;
+        }
+        return count;
     }
     public int numSubarraysWithSum(int[] nums, int goal) {
        return minfun(nums,goal)-minfun(nums,goal-1);
