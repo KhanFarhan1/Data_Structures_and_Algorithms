82cc226faef06c389b21f1e5921572363313e0f1
Farhan Khan <farhan@eng.rizvi.edu.in>
2026-08-11 16:05:08 +0530

    Time: 3 ms (13.95%), Space: 47.1 MB (57.1%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[][] insert(int[][] nums, int[] newInterval) {
+        ArrayList<int []> res = new ArrayList<>();
+        Boolean insert = false;
+        for(int i =0;i<nums.length;i++){
+            int start = nums[i][0];
+            if(insert==false && start>=newInterval[0]){
+                res.add(new int[]{newInterval[0],newInterval[1]});
+                insert = true;
+            }
+            res.add(new int[]{nums[i][0],nums[i][1]});
+        }
+        if(insert == false){
+            res.add(new int[]{newInterval[0],newInterval[1]});
+        }
+        ArrayList<int []> ans = new ArrayList<>();
+        int start1 = res.get(0)[0];
+        int end1 = res.get(0)[1];
+        for(int i = 1;i<res.size();i++){
+            int start2 = res.get(i)[0];
+            int end2 = res.get(i)[1];
+            if(end1>=start2){
+                end1 = Math.max(end1,end2);
+            }else{
+                ans.add(new int[]{start1,end1});
+                start1 = start2;
+                end1=end2;
+            }
+        }
+        ans.add(new int[]{start1,end1});
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
