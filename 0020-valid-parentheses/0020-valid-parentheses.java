9236f97dacce0909f27caec99178d1a279473b90
Farhan Khan <farhan@eng.rizvi.edu.in>
2026-08-18 01:37:15 +0530

    Time: 4 ms (36.67%), Space: 43.3 MB (58.12%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(st.isEmpty()){
+                st.push(c);
+            }else{
+                char d = st.peek();
+                if((d == '[' && c == ']')||(d == '{' && c == '}')||(d == '(' && c == ')')){
+                    st.pop();
+                }else{
+                    st.push(c);
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
