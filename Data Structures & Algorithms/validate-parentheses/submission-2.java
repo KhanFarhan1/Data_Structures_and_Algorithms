bdf935da37cf22bead6f696aab9e4c170eac5991
Farhan Khan <farhan@eng.rizvi.edu.in>
2026-08-18 01:33:23 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        int n = s.length();
+        for(int i = 0;i<n;i++){
+            char c = s.charAt(i);
+            if(st.isEmpty()){
+                st.push(c);
+            }else{
+                char d = st.peek();
+                if((d == '[' && c ==']') || (d == '(' && c == ')') || (d == '{' && c == '}')){
+                    st.pop();
+                }else{
+                    st.push(c);
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
