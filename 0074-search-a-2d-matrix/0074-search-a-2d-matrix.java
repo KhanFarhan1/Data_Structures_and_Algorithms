9b666c9dd55abd635e375e8d9d9deb5a5945af59
Farhan Khan <farhan@eng.rizvi.edu.in>
2026-09-14 14:28:56 +0530

    Time: 60 ms (0.78%), Space: 44.1 MB (8.83%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,43 +1,43 @@
 class Solution {
-    public boolean binary_serach(int[] arr , int target){
-        int si = 0;
-        int ei = arr.length-1;
-        while(si<=ei){
-            int mid = si +(ei -si) /2;
-            if(arr[mid] == target){
-                return true;
-            }
-            if(arr[mid] > target){
-                ei = mid-1;
-            }else{
-                si = mid+1;
-            }
-        }
-        return false;
-    }
-    public boolean searchMatrix(int[][] matrix, int target) {
-       int m = matrix.length;
-       int n = matrix[0].length;
-       int si = 0;
-       int ei = m-1;
-       if(m == 1){
-        return  binary_serach(matrix[0] , target);
-       }
-       while(si <= ei){
-        int mid = si + (ei-si)/2;
-        if(matrix[mid][0]== target){
-            return true;
-        }
-        if((matrix[mid][0] <= target) && (target <= matrix[mid][n-1])){
-            return binary_serach(matrix[mid] , target);
-        }
-        if((matrix[mid][0] <target)&& (target >matrix[mid][n-1])){
-            si = mid+1;
-        }else{
-            ei = mid-1;
-        }
-       }
-       return false;
+    // public boolean binary_serach(int[] arr , int target){
+    //     int si = 0;
+    //     int ei = arr.length-1;
+    //     while(si<=ei){
+    //         int mid = si +(ei -si) /2;
+    //         if(arr[mid] == target){
+    //             return true;
+    //         }
+    //         if(arr[mid] > target){
+    //             ei = mid-1;
+    //         }else{
+    //             si = mid+1;
+    //         }
+    //     }
+    //     return false;
+    // }
+    // public boolean searchMatrix(int[][] matrix, int target) {
+    //    int m = matrix.length;
+    //    int n = matrix[0].length;
+    //    int si = 0;
+    //    int ei = m-1;
+    //    if(m == 1){
+    //     return  binary_serach(matrix[0] , target);
+    //    }
+    //    while(si <= ei){
+    //     int mid = si + (ei-si)/2;
+    //     if(matrix[mid][0]== target){
+    //         return true;
+    //     }
+    //     if((matrix[mid][0] <= target) && (target <= matrix[mid][n-1])){
+    //         return binary_serach(matrix[mid] , target);
+    //     }
+    //     if((matrix[mid][0] <target)&& (target >matrix[mid][n-1])){
+    //         si = mid+1;
+    //     }else{
+    //         ei = mid-1;
+    //     }
+    //    }
+    //    return false;
     // public boolean searchMatrix(int[][] matrix, int target) {
     //     int n = matrix[0].length;
     //     int si = 0;
@@ -59,5 +59,24 @@ class Solution {
     //         return false;
     //     }
     //     return binary_serach(matrix[predicted_row] , target);
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int si = 0;
+        int ei = (m*n)-1;
+        while(si <= ei){
+            int mid = si + (ei - si)/2;
+            int row = mid/n;
+            int col = mid%n;
+            if(matrix[row][col] == target){
+                return true;
+            }
+            if(matrix[row][col] > target){
+                ei = mid-1;
+            }else{
+                si = mid+1;
+            }
+        }
+        return false;
     }
 }
